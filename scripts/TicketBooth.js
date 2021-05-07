c474b4d7b4bb2a3f236397799815877c6106af89
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-05-07 12:00:11 -0500

    Checking for event listeners

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,13 +1,22 @@
-const contentTarget = document.querySelector(".entry")
-const eventHub = document.querySelector("#state-fair")
+const contentTarget = document.querySelector(".entry");
+const eventHub = document.querySelector("#state-fair");
 
-eventHub.addEventListener()
+eventHub.addEventListener();
 
 export const TicketBooth = () => {
-    contentTarget.innerHTML = `
+  contentTarget.innerHTML = `
         <div class="ticketBooth">
-            Add button here
+            <button id="rideTicket">Ride Ticket</button>
         </div>
-    `
-}
+    `;
+};
 
+const rideTicketSelectedEvent = new CustomEvent("ticketSelected", {
+  detail: {
+    ticketSelected: "rideTicket",
+  },
+});
+
+eventHub.addEventListener(rideTicketSelectedEvent);
+
+const rideEvent = new CustomEvent("rideTicketPurchased");
